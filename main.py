d1590b6c05eb27dddc487293bdc4c6239ecc431f
Stephen J Smith <35276198+kn13htmare@users.noreply.github.com>
2021-12-06 02:57:19 +0000

    Update main.py

--- main.py
+++ main.py
@@ -23,19 +23,14 @@ def soaker():
 def calc():
     st = request.args.get("start_time", "")
     dur = request.args.get("duration")
-    #print(str(st))
-    #print(str(dur))
     stc = (str(st))
     durc = (str(dur))
     formatter = '%H:%M'
     dt = datetime.strptime(stc, formatter)
     soaktime = timedelta(minutes=+int(durc))
     endsoak = dt + soaktime
-    #Print just the time
     result = endsoak.strftime("%H:%M")
     disp_result.append(result)
-    #print(endsoak.strftime("%H:%M"))
-    #print(result) 
     
 
 if __name__ == "__main__":
